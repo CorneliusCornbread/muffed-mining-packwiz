e9e62f6888ccd7e7515a2c9881fe72b5baec5090
CorneliusCorn <jrosenbeckerdev@gmail.com>
2026-08-18 16:33:42 -0500

    Add tannery block fixes

---
+++ kubejs/server_scripts/tannery/table_fix.js
@@ -0,0 +1,13 @@
+// Make the tanning table actually drop itself
+LootJS.lootTables(event => {
+    event.create("tannery:blocks/tanning_table", LootType.BLOCK)
+        .createPool(pool => {
+            pool.rolls(1)
+            pool.addEntry(LootEntry.of("tannery:tanning_table").setCount([1, 1]))
+        })
+});
+
+// Make drying racks mineable with axe
+ServerEvents.tags('block', event => {
+    event.add('minecraft:mineable/axe', 'tannery:tanning_rack');
+});
