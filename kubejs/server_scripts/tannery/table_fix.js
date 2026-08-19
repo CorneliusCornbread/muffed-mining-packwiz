53e434361bef79df33fc963da66b6dc192118926
CorneliusCorn <jrosenbeckerdev@gmail.com>
2026-08-18 21:47:32 -0500

    Add patched tree physics for now

--- kubejs/server_scripts/tannery/table_fix.js
+++ kubejs/server_scripts/tannery/table_fix.js
@@ -1,10 +1,12 @@
 // Make the tanning table actually drop itself
 LootJS.lootTables(event => {
-    event.create("tannery:blocks/tanning_table", LootType.BLOCK)
-        .createPool(pool => {
-            pool.rolls(1)
-            pool.addEntry(LootEntry.of("tannery:tanning_table").setCount([1, 1]))
-        })
+    if (!event.hasLootTable("tannery:blocks/tanning_table")) {
+        event.create("tannery:blocks/tanning_table", LootType.BLOCK)
+            .createPool(pool => {
+                pool.rolls(1)
+                pool.addEntry(LootEntry.of("tannery:tanning_table").setCount([1, 1]))
+            })
+    }
 });
 
 // Make drying racks mineable with axe
